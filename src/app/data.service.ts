fcf8fb22113a1a1273360ebbefc48d6b27ddf187
rocket1kumar <rocket1kumar.live@gmail.com>
2018-08-02 15:43:33 +0530

    initial commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {Http, Response} from "@angular/http";
+import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import { Customer } from './customer.model';
+//import { CUSTOMERS } from './customers';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class DataService {
+
+  constructor( private http: HttpClient) { }
+	
+
+
+
+saveCustomer(customer){
+	return this.http.post("http://localhost:8080/api/savecustomer",customer);//.map(res => { return res;});
+}
+
+getData(){
+	return this.http.get("http://localhost:8080/api/customers");
+
+}
+
+deleteCustomerById(id){
+	 return this.http.delete("http://localhost:8080/api/delete/"+id);
+}
+
+getDataById(id){
+	return this.http.get("http://localhost:8080/api/customer/"+id);
+}
+
+}
+
+//end 
+
