61d3e6ffca7edcb3c3ff8ac9753adf43b7631612
Vipul Kumar Maurya <rocket1kumar.live@gmail.com>
2018-08-20 11:12:37 +0530

    Update data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Http, Response} from "@angular/http";
+//import {Http, Response} from "@angular/http";
 import 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import { Customer } from './customer.model';
